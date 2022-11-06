282e0c68e3bc6bba91667bd4e4c32d5ec9a7c6dd
Salvador Rodríguez <salvarodriguezabad@gmail.com>
2022-11-06 12:55:44 +0100

    176SecondHighestSalary

---
+++ 176SecondHighestSalary.sql
@@ -0,0 +1,19 @@
+-- Input: 
+-- Employee table:
+-- +----+--------+
+-- | id | salary |
+-- +----+--------+
+-- | 1  | 100    |
+-- | 2  | 200    |
+-- | 3  | 300    |
+-- +----+--------+
+-- Output: 
+-- +---------------------+
+-- | SecondHighestSalary |
+-- +---------------------+
+-- | 200                 |
+-- +---------------------+
+
+SELECT MAX(salary) AS 'SecondHighestSalary'
+FROM Employee
+WHERE salary < (SELECT MAX(salary) FROM Employee)
